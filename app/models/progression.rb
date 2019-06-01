2d6fe794cbfe714a28995d2212c5d9d1bc0070b3
anw0514 <anw0514@gmail.com>
2019-06-01 17:25:23 -0400

    relationships through others

--- app/models/progression.rb
+++ app/models/progression.rb
@@ -1,3 +1,5 @@
 class Progression < ApplicationRecord
   belongs_to :music, polymorphic: true
+  has_many :chord_progressions
+  has_many :chords, through: :chord_progressions
 end
--- app/models/subsection.rb
+++ app/models/subsection.rb
@@ -1,4 +1,7 @@
 class Subsection < ApplicationRecord
+    belongs_to :song
     has_many :links, as: :music
     has_many :progressions, as: :music
+    has_many :chord_progressions, through: :progressions
+    has_many :chords, through: :chord_progressions
 end
