531840d828b12a6a15c79a3fd1dadd56773833d5
anw0514 <anw0514@gmail.com>
2019-06-02 19:25:18 -0400

    song seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Progression.destroy_all
+Song.destroy_all
+Subsection.destroy_all
+Chord.destroy_all
+ChordProgression.destroy_all
+Link.destroy_all
+
+
+# Songs
+ms = Song.create(title: 'Maybe Soon', key: 'G', lyrics: "Maybe soon / I'll forget / all the pain / and the stress" )
+nevermind = Song.create(title: 'Nevermind', key: 'Unknown', lyrics: "Now I / I find / Solace / / You Wont / Ever / Promise")
+
+# Subsections
+
+# 
