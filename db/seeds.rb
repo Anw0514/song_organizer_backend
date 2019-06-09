6576335ad1aa4561d34c1c35b68027fe7646689c
anw0514 <anw0514@gmail.com>
2019-06-09 17:15:00 -0400

    progression seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,18 +20,34 @@ nevermind = Song.create(title: 'Nevermind', key: 'Unknown', lyrics: "Now I / I f
 
 # Chords
 a = Chord.create(letter: 'A', key: 'major')
+am = Chord.create(letter: 'A', key: 'minor')
 b = Chord.create(letter: 'B', key: 'major')
 c = Chord.create(letter: 'C', key: 'major')
 d = Chord.create(letter: 'D', key: 'major')
 e = Chord.create(letter: 'E', key: 'major')
+em = Chord.create(letter: 'E', key: 'minor')
 f = Chord.create(letter: 'F', key: 'major')
 g = Chord.create(letter: 'G', key: 'major')
+gmajsev = Chord.create(letter: 'G', key: 'major', addition: 'maj7')
+gsev = Chord.create(letter: 'G', key: 'major', addition: '7')
 ab = Chord.create(letter: 'Ab', key: 'major')
 bb = Chord.create(letter: 'Bb', key: 'major')
 bbsev = Chord.create(letter: 'Bb', key: 'major', addition: '7')
 
 # Links
 Link.create(song: ms, text: 'https://www.youtube.com/embed/LfaU-KEkBt8')
+
+# Progressions & chords
+h = Progression.create(song: nevermind)
+ChordProgression.create(progression: h, chord: em)
+ChordProgression.create(progression: h, chord: am)
+
+i = Progression.create(song: ms)
+ChordProgression.create(progression: i, chord: g)
+ChordProgression.create(progression: i, chord: gmajsev)
+ChordProgression.create(progression: i, chord: gsev)
+ChordProgression.create(progression: i, chord: c)
+
 
 
 
