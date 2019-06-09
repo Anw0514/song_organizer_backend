8f6adbf22592898dada7f30b0d2b9b2c8846e8ce
anw0514 <anw0514@gmail.com>
2019-06-09 17:00:15 -0400

    removed semi-duplicate column from links

--- db/migrate/20190530114020_create_links.rb
+++ db/migrate/20190530114020_create_links.rb
@@ -5,7 +5,6 @@ class CreateLinks < ActiveRecord::Migration[5.2]
       t.string :note
       t.integer :minutes
       t.integer :seconds
-      t.string :notes
       t.belongs_to :song
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -35,7 +35,6 @@ ActiveRecord::Schema.define(version: 2019_05_30_114601) do
     t.string "note"
     t.integer "minutes"
     t.integer "seconds"
-    t.string "notes"
     t.integer "song_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
